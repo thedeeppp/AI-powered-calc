6639740cc0bffa719a94c904e6161bc59d33dc2c
Deep Trivedi <deepbtrivedi04@gmail.com>
2024-12-13 15:42:04 +0530

    final setup

---
+++ frontend/src/main.tsx
@@ -0,0 +1,13 @@
+// import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { StrictMode } from 'react'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+// ReactDOM.render(<App/>, document.getElementById('root'))
